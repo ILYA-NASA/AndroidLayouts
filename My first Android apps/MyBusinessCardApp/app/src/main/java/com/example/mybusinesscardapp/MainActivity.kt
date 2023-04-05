3cb1f68a41827b4578155f32c24aea8f9531618d
ILYA.NASA <il.nasa@outlook.com>
2023-04-05 15:27:52 +0300

    added contacts info

--- My first Android apps/MyBusinessCardApp/app/src/main/java/com/example/mybusinesscardapp/MainActivity.kt
+++ My first Android apps/MyBusinessCardApp/app/src/main/java/com/example/mybusinesscardapp/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.example.mybusinesscardapp
 
 import android.os.Bundle
-import android.text.style.BackgroundColorSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -18,9 +16,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.mybusinesscardapp.ui.theme.MyBusinessCardAppTheme
 
@@ -46,7 +42,11 @@ fun ComposeMyBusinessCard() {
     MyBusinessCard(
         fullName = stringResource(R.string.my_name),
         currentStatus = stringResource(R.string.my_status),
+        telephone = stringResource(R.string.my_telephone),
+        telegram = stringResource(R.string.my_telegram),
+        mail = stringResource(R.string.my_mail),
         avatar = painterResource(R.drawable.android_logo),
+        googleProfile = painterResource(R.drawable.my_qrcode_developers_google_com),
         backgroundColor = Color.Black,
     )
 }
@@ -55,7 +55,11 @@ fun ComposeMyBusinessCard() {
 private fun MyBusinessCard(
     fullName: String,
     currentStatus: String,
+    telephone: String,
+    telegram: String,
+    mail: String,
     avatar: Painter,
+    googleProfile: Painter,
     backgroundColor: Color,
 ) {
     Column(
@@ -82,6 +86,24 @@ private fun MyBusinessCard(
             color = Color(0xFF3ddc84),
 //            modifier = Modifier
         )
+        Text(
+            text = telephone,
+            fontSize = 17.sp,
+            color = Color.Gray
+        )
+        Text(
+            text = telegram,
+            fontSize = 17.sp,
+            color = Color.Gray
+        )
+        Text(
+            text = mail,
+            fontSize = 17.sp,
+            color = Color.Gray
+        )
+        Image(
+            painter = googleProfile,
+            contentDescription = null)
     }
 }
 
